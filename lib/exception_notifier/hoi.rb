f1cc89bfc90877a18fca7834c42358b525464445
Hiroshi Yoshida <hrysd22@gmail.com>
2014-06-20 22:26:54 +0900

    For codeクリミネイト

---
+++ lib/exception_notifier/hoi.rb
@@ -0,0 +1,17 @@
+class Hoi
+  def initialize(args)
+    if args > 1
+      if args > 2
+        if args > 3
+          if args > 4
+            if args > 5
+              puts args if args < 10
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+Hoi.new(8)
